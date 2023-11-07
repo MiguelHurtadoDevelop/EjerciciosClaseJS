e44a4a331168508c5a6c91292bd0b4515de4962e
MiguelHurtado <mhurtadojunior@gmail.com>
2023-11-08 00:59:08 +0100

    PongGame

---
+++ PongGame/PongGame.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    const svg = document.getElementById("pongSVG");
+    const paddle1 = document.getElementById("paddle1");
+    const paddle2 = document.getElementById("paddle2");
+    const ball = document.getElementById("ball");
+    
+}
